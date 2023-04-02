938059aac6cb7729d77aabe51ea3dc751854b1bc
tmadeline <128552919+tmadeline@users.noreply.github.com>
2023-04-02 12:43:09 -0400

    Update

--- templates/menu.js
+++ templates/menu.js
@@ -1,26 +1,5 @@
-const menuButton = document.getElementById('header-mobile__menu-toggle');
-const menu = document.getElementById('header-mobile__menu');
+const hamburger = document.querySelector('.hamburger');
 
-// Function to toggle the menu
-function toggleMenu() {
-  menu.classList.toggle('show');
-  // Check if menu is open
-  if (menu.classList.contains('show')) {
-    // Replace menu icon with animation
-    menuButton.innerHTML = '<img src="hamxin.gif" alt="Menu">';
-    // Wait for animation to finish and replace with icon
-    setTimeout(function() {
-      menuButton.innerHTML = '<img src="hamburger.png" alt="Menu">';
-    }, 1000);
-  } else {
-    // Replace menu icon with animation
-    menuButton.innerHTML = '<img src="hamxout.gif" alt="Menu">';
-    // Wait for animation to finish and replace with icon
-    setTimeout(function() {
-      menuButton.innerHTML = '<img src="hamburger.png" alt="Menu">';
-    }, 1000);
-  }
-}
-
-// Add event listener to menu button
-menuButton.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('change');
+});
--- urls.py
+++ urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from portfolio import views
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
