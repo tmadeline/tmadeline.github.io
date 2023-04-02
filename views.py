e1380dd24b61391a06b7bbf891e77e2e1f342c78
tmadeline <128552919+tmadeline@users.noreply.github.com>
2023-04-02 11:00:41 -0400

    Update

--- views.py
+++ views.py
@@ -1,31 +1,39 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def home(request):
-    context = {'title': 'Welcome!'}
-    return render(request, './templates/home.html', context)
+    now = datetime.now()
+    if 0 <= now.hour < 12:
+        greeting = 'good morning'
+    elif 12 <= now.hour < 18:
+        greeting = 'good afternoon'
+    else:
+        greeting = 'good evening'
+    context = {'title': 'Welcome!'}, {'greeting': greeting}
+    return render(request, 'home.html', context)
 
 
 def about(request):
     context = {'title': 'About Me'}
-    return render(request, './templates/about.html', context)
+    return render(request, 'about.html', context)
 
 
 def projects(request):
     context = {'title': 'Projects'}
-    return render(request, './templates/projects.html', context)
+    return render(request, 'projects.html', context)
 
 
 def certifications(request):
     context = {'title': 'Certifications'}
-    return render(request, './templates/certifications.html', context)
+    return render(request, 'certifications.html', context)
 
 
 def references(request):
     context = {'title': 'References'}
-    return render(request, './templates/references.html', context)
+    return render(request, 'references.html', context)
 
 
 def contact(request):
     context = {'title': 'Contact Me'}
-    return render(request, './templates/contact.html', context)
+    return render(request, 'contact.html', context)
