1c06966cdca1de0b8060f2db7e3a0a2a9a014736
tmadeline <128552919+tmadeline@users.noreply.github.com>
2023-04-01 20:38:54 -0400

    PyCharm Initial Trial

---
+++ portfolio/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+
+def home(request):
+    context = {'title': 'Home Page'}
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {'title': 'About Me'}
+    return render(request, 'about.html', context)
+
+
+def projects(request):
+    context = {'title': 'Projects'}
+    return render(request, 'projects.html', context)
+
+
+def certifications(request):
+    context = {'title': 'Certifications'}
+    return render(request, 'certifications.html', context)
+
+
+def references(request):
+    context = {'title': 'References'}
+    return render(request, 'references.html', context)
+
+
+def contact(request):
+    context = {'title': 'Contact Me'}
+    return render(request, 'contact.html', context)
